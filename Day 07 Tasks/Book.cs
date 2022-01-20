8756b4ebe00f0c46fe41fa86a0ea5a998f2da733
romioshoula <romioshoula@gmail.com>
2022-01-20 11:00:22 +0200

    Day 07 Tasks

---
+++ Day 07 Tasks/Book.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_Book
+{
+    public delegate O GenericBDelegate<in T, out O>(T X);
+    public delegate string AlphaDelegate(Book book);
+    public class Book
+    {
+        private string v1;
+        private string v2;
+        private string[] vs;
+        private Func<string> toString;
+        private int v3;
+
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+        public Book(string _ISBN, string _Title,
+        string[] _Authors, DateTime _PublicationDate,
+       decimal _Price)
+        {
+            ISBN = _ISBN;
+            Title = _Title;
+            Authors = _Authors;
+            PublicationDate = _PublicationDate;
+            Price = _Price;
+        }
+
+        public Book(string v1, string v2, string[] vs, Func<string> toString, int v3)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.vs = vs;
+            this.toString = toString;
+            this.v3 = v3;
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class BookFunctions
+    {
+        public static string GetTitle(Book B)
+        {
+            return B.Title;
+        }
+        public static string GetAuthors(Book B)
+        {
+            StringBuilder s = new StringBuilder("", 100);
+            for (int i = 0; i < B.Authors?.Length; i++)
+            {
+                s.Append($"{B.Authors[i]} ,");
+            }
+            return s.ToString();
+        }
+        public static string GetPrice(Book B)
+        {
+            return B.Price.ToString();
+        }
+    }
+    public class LibraryEngine
+    {
+        public static void ProcessBooks(List<Book> bList
+       , AlphaDelegate fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+
+        public static void ProcessBooks(List<Book> bList
+       , Func<Book, String> fPtr)
+        {
+            foreach (Book B in bList)
+            {
+                Console.WriteLine(fPtr(B));
+            }
+        }
+    }
+}
---
+++ Day 07 Tasks/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LibraryEngine libraryEngine = new LibraryEngine();
+            Book book1 = new Book("ISBN1", "C# Book 1", new string[1] { "Rami" }, DateTime.Now, 100);
+            Book book2 = new Book("ISBN2", "C# Book 2", new string[2] { "Rami", "Ahmed" }, DateTime.Now, 120);
+            Book book3 = new Book("ISBN3", "C# Book 3", new string[3] { "Rami", "Ahmed", "Khaled" }, DateTime.Now, 300);
+
+            List<Book> books = new List<Book>();
+            books.Add(book1);
+            books.Add(book2);
+            books.Add(book3);
+
+            Console.WriteLine("Delegates using fPtr implementation in C#:");
+            Console.WriteLine("");
+
+            Console.WriteLine("User Defined Delegate Datatype:");
+            Console.WriteLine("Book Price:");
+            AlphaDelegate fptr = new AlphaDelegate(BookFunctions.GetPrice);
+            LibraryEngine.ProcessBooks(books, fptr);
+            Console.WriteLine("");
+
+            /**/
+            Console.WriteLine("BCL Delegates:");
+            Console.WriteLine("Book Price:");
+            Func<Book, String> fptrBCL = new Func<Book, String>(BookFunctions.GetPrice);
+            LibraryEngine.ProcessBooks(books, fptrBCL);
+            Console.WriteLine("");
+
+            Console.WriteLine("Anonymous Function Methods (GetISBN):");
+            AlphaDelegate fptrAn = delegate (Book Alpha) { return Alpha.ISBN; };
+            LibraryEngine.ProcessBooks(books, fptrAn);
+            Console.WriteLine("");
+
+
+            //
+            Console.WriteLine("Lambda Expression (GetPublicationTitle):");
+            AlphaDelegate fptrLambda = (Alpha) => Alpha.Title;
+            LibraryEngine.ProcessBooks(books, fptrLambda); //DateTime.Now
+            Console.WriteLine("");
+            Console.WriteLine("Printing Date&Time:");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("");
+
+            /**/
+            Console.WriteLine("Authors:");
+            Func<Book, String> fptrAuth = new Func<Book, String>(BookFunctions.GetAuthors);
+            LibraryEngine.ProcessBooks(books, fptrAuth);
+            Console.WriteLine("");
+
+        }
+    }
+}
